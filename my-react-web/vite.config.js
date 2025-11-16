b59bb2e184b15f28feccc81ae81afcc343cebdb1
tarrut <91079521+tarrut@users.noreply.github.com>
2025-11-16 17:29:56 +0100

    changes

--- my-react-web/vite.config.js
+++ my-react-web/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/web_comiats/", // 👈 change this to your repo name
+  plugins: [react()]
 });
